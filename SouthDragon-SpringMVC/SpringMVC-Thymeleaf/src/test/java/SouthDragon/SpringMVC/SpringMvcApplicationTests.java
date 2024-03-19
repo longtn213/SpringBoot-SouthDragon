a1c48491e909990ae7d0fdee2b53bf8a497bc102
DESKTOP-IQA7J21\tnaml <tnamlong2132000@gmail.com>
2024-03-20 00:25:36 +0700

    Spring MVC Thymeleaf

---
+++ SouthDragon-SpringMVC/SpringMVC-Thymeleaf/src/test/java/SouthDragon/SpringMVC/SpringMvcApplicationTests.java
@@ -0,0 +1,13 @@
+package SouthDragon.SpringMVC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
