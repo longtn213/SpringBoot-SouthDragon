9c981f65563e8856fa23e92a8a29dee5bb6abda6
DESKTOP-IQA7J21\tnaml <tnamlong2132000@gmail.com>
2024-04-02 18:46:25 +0700

    springMVC Security plain-text

---
+++ SouthDragon-SpringMVC/SpringMVCSecurity/src/test/java/SouthDragon/SpringMVCSecurity/SpringMvcSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package SouthDragon.SpringMVCSecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringMvcSecurityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
